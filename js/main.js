b151efead47a948a9a733df95d583a11704fc88f
yukiii0229 <92935326+yukiii0229@users.noreply.github.com>
2021-11-12 19:04:15 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const target = document.getElementById("menu");
 target.addEventListener('click', () => {
-  const target = document.getElementById("menu");
   target.classList.toggle('open');
   const nav = document.getElementById("drawer-nav");
   nav.classList.toggle('in');
